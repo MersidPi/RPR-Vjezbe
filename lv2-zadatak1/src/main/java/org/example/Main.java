eda6ebdada3866eb9eb78c87f2e0a67dd806d774
Mersid Pilipović <115986576+MersidPi@users.noreply.github.com>
2023-10-30 08:46:39 +0100

    gotov zadatak 1 iz lv2

--- lv2-zadatak1/src/main/java/org/example/Main.java
+++ lv2-zadatak1/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] ARGS) {
+        String[] args = new String[2];
+        System.out.println("Upisite sinus ili faktorijel: ");
+        Scanner s = new Scanner(System.in);
+        args[0] = s.nextLine();
+        System.out.println("Upisite broj: ");
+        args[1] = s.nextLine();
         if(args[0].equals("sinus"))
             System.out.println("rezultat je: " + Matematika.sinus(Double.parseDouble(args[1])));
         else if (args[0].equals("faktorijel"))
